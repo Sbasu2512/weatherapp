623c09513c5d54785e6f2c7ab428865bdeea784a
sayantanbasu <sayantanbasu@live.in>
2022-05-05 09:01:45 +0530

    cards created, layout changed

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import Head from 'next/head';
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import '../styles/custom.css'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <meta name='viewport' content="width=device-width, initial-scale=1" />
+    </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Container } from 'react-bootstrap'
-import styles from '../styles/Home.module.css'
 import {Container,Row, Col} from 'react-bootstrap'
+import Dashboard from '../components/Sidebar/Dashboard'
+
 
 export default function Home() {
   return (
-    <div className='container'>
-     <Container>
-     <Row>
-        <Col>
-        
-        </Col>
-        </Row>
-     </Container>
+    <div>
+    <Row>
+Top Part
+    </Row>
+    <Row>
+      bottom part
+    </Row>
     </div>
   )
 }
